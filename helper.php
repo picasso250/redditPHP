1c9d4c1a23298de8da2fcfc5ef4d3e5430736894
wangxiaochi <wangxiaochi@appcpx.com>
2018-11-15 19:04:11 +0800

    z

--- helper.php
+++ helper.php
@@ -1,2 +1,12 @@
 <?php
 
+function cmt($cid)
+{
+    $redis = $GLOBALS['containner']->get('Redis');
+    return json_decode($redis->get("c:$cid"));
+}
+function cmt_child($cid)
+{
+    $redis = $GLOBALS['containner']->get('Redis');
+    return $redis->zRevRangeByScore("cmt_child:$cid", '+inf', 0, ['withscores' => TRUE]);
+}
