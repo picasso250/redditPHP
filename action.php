b2f4734b382370802bc1ba11b1088e24f103daf1
wangxiaochi <wangxiaochi@appcpx.com>
2018-11-15 19:08:12 +0800

    z

--- action.php
+++ action.php
@@ -8,10 +8,13 @@ function action_r($name)
 {
     $redis = $GLOBALS['containner']->get('Redis');
     $lst = $redis->zRevRangeByScore("r:$name", '+inf', 0, ['withscores' => TRUE]);
+    $_inner_tpl = "r.php";
     include ROOT_VIEW.'/layout.php';
 }
-function action_comment()
+function action_comment($r, $id)
 {
-
+    $redis = $GLOBALS['containner']->get('Redis');
+    $score = $redis->zScore("r:$r", $id);
+    $_inner_tpl = "comment.php";
     include ROOT_VIEW.'/layout.php';
 }
---
+++ view/layout.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="zh-cmn-Hans">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>A example of Step-PHP</title>
+
+    <meta name="author" content="xiaochi">
+
+    <meta name="HandheldFriendly" content="true">
+    <meta name="apple-mobile-web-app-title" content="Step-PHP">
+
+</head>
+
+<body>
+    <?php include $_inner_tpl; ?>
+</body>
+
+</html>
